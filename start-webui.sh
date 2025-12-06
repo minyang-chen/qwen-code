579a4ed27e01bf8b7457225b161212c67acd9e0f
mychen76 <mychen76@gmail.com>
2025-12-05 20:44:48 -0500

    clean up

--- start-webui.sh
+++ start-webui.sh
@@ -11,11 +11,7 @@ echo ""
 echo "Press Ctrl+C to stop"
 echo ""
 
-export OPENAI_BASE_URL="http://10.0.0.82:8080/v1"
-export OPENAI_API_KEY="FREE"
-export OPENAI_MODEL="gpt3.5-turbo"
 
 cd "$(dirname "$0")"
-#OPENAI_BASE_URL="https://api.openai.com/v1" OPENAI_MODEL="gpt-4" npm run web-ui:dev
 
-OPENAI_BASE_URL="http://10.0.0.82:8080/v1" OPENAI_API_KEY="FREE" OPENAI_MODEL="gpt3.5-turbo" npm run web-ui:dev
+npm run web-ui:dev
